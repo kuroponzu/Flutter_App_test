7cd7307e9366d3d49dbbc2a5f64ea0c26cf2141d
sho shimohata <pontatanpo1014@gmail.com>
2018-08-17 02:52:43 +0900

    レイアウト修正

--- lib/main.dart
+++ lib/main.dart
@@ -4,127 +4,74 @@ void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Flutter Demo',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: new InputForm(title: '入力画面'),
-    );
+
+    return MaterialApp(
+      title: "FlutterDemo",
+//      home: Scaffold(
+//        appBar: AppBar(
+//          title: Text("FlutterDemo"),
+//        ),
+        home: new InputForm(),
+            //textSection,
+      );
   }
 }
 
 class InputForm extends StatefulWidget {
   InputForm({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
-
   @override
   _MyInputFormState createState() => new _MyInputFormState();
 }
 
 class _MyInputFormState extends State<InputForm> {
+//  final _formkey = GlobalKey<FormState>();
 
-  final _formkey = GlobalKey<FormState>();
-  final _myController = TextEditingController();
-  final _myController2 = TextEditingController();
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-    });
-  }
+//  @override
+ Widget build(BuildContext context) {
+   final _myController = TextEditingController();
+   final _myController2 = TextEditingController();
 
-  @override
-  Widget build(BuildContext context) {
+   Widget titleSection = Scaffold(
+       body: new SafeArea(
+           child: new Form(
+               child: new ListView(
+                 padding: const EdgeInsets.all(20.0),
+                   children: <Widget>[
+//               new Flexible(
+//                 child: Column(
+//                   children: <Widget>[
+//                     Container(
+//                       child: Text("相手:"),
+//                     ),
+//                   ],)
+//             ),
+//             new Flexible(
+//               fit: FlexFit.loose,
+                     new TextFormField(
+                       controller: _myController,
+                       decoration: const InputDecoration(
+                         icon: const Icon(Icons.person),
+                         hintText: '名前',
+                         labelText: 'Name',
+                       ),
+                     ),
 
+                     new Container(
+                         padding: const EdgeInsets.only(left: 40.0, top: 20.0),
+                         child: new RaisedButton(
+                           child: const Text('Submit'),
+                           onPressed: null,
+                         )
+                     )
+                   ]
+               ))
+       )
+   );
+   return titleSection;
+ }
 
-
-
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return new Scaffold(
-      appBar: new AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: new Text(widget.title),
-      ),
-      body: new Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: new Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug paint" (press "p" in the console where you ran
-          // "flutter run", or select "Toggle Debug Paint" from the Flutter tool
-          // window in IntelliJ) to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-
-
-          children: <Widget>[
-
-            new Text(
-                "相手"
-            ),
-            new TextFormField(
-                controller: _myController
-                ),
-            new Text(
-                "対象"
-            ),
-            new TextFormField(
-                controller: _myController2
-            ),
-            Padding (
-              padding: const EdgeInsets.symmetric(vertical: 16.0),
-              child: RaisedButton(
-                onPressed: (){
-                    print(_myController.text);
-                    print(_myController2.text);
-                },
-                child: Text("登録"),
-              ),
-            )
-          ],
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: new Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
-  }}
+}
